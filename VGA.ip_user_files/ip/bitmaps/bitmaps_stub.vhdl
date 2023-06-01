a094d6d92fbd560a920c7228148518210968c23e
samuelpeter17 <96541225+samuelpeter17@users.noreply.github.com>
2023-06-01 16:03:54 -0400

    Add files via upload

---
+++ VGA.ip_user_files/ip/bitmaps/bitmaps_stub.vhdl
@@ -0,0 +1,32 @@
+-- Copyright 1986-2021 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2021.2 (win64) Build 3367213 Tue Oct 19 02:48:09 MDT 2021
+-- Date        : Wed May 31 17:39:32 2023
+-- Host        : Latitude-3410 running 64-bit major release  (build 9200)
+-- Command     : write_vhdl -force -mode synth_stub o:/ENGS31_S23/VGA/VGA.gen/sources_1/ip/bitmaps/bitmaps_stub.vhdl
+-- Design      : bitmaps
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7a35tcpg236-1
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity bitmaps is
+  Port ( 
+    clka : in STD_LOGIC;
+    ena : in STD_LOGIC;
+    addra : in STD_LOGIC_VECTOR ( 7 downto 0 );
+    douta : out STD_LOGIC_VECTOR ( 63 downto 0 )
+  );
+
+end bitmaps;
+
+architecture stub of bitmaps is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "clka,ena,addra[7:0],douta[63:0]";
+attribute x_core_info : string;
+attribute x_core_info of stub : architecture is "blk_mem_gen_v8_4_5,Vivado 2021.2";
+begin
+end;
